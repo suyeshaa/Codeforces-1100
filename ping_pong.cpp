545fc55d9f29a5d40a398d49eb2681431db8d5d4
Suyesha Chaudhary <suyeshaachaudhry@gmail.com>
2021-08-14 23:42:31 +0530

    codeforces

--- ping_pong.cpp
+++ ping_pong.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void solve()
+{
+    long x, y;
+    cin >> x >> y;
+
+        cout << x - 1 << " " << y << endl;
+}
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        long x, y;
-        cin >> x >> y;
+        solve();
     }
 }
